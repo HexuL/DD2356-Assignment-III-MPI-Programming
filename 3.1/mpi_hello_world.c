8167125c44c6f2e8b1faa6dc619a9c57b0509884
RedBirdYH <143439528+RedBirdYH@users.noreply.github.com>
2024-05-04 13:07:54 +0200

    Create mpi_hello_world.c

---
+++ 3.1/mpi_hello_world.c
@@ -0,0 +1,21 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    // Initialize the MPI environment
+    MPI_Init(&argc, &argv);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Print off a hello world message
+    printf("Hello World from rank %d from %d processes!\n", world_rank, world_size);
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
+}
